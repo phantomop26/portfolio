e6e5c1f3e66cd671be16bfc407771ae6f71e4c68
funda <funda@Sahils-MacBook-Air.local>
2025-07-30 21:23:15 -0400

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true, // Optional but recommended for static hosting
 };
 
 export default nextConfig;
